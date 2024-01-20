296c850010f1d2574ebdeba1e0a6d222f14d7ba3
Guri <gsthiara04101999@gmail.com>
2024-01-20 18:52:13 +0530

    improvements

--- src/pages/Sidebar/SidebarItems/CreateModel/CreateModal.jsx
+++ src/pages/Sidebar/SidebarItems/CreateModel/CreateModal.jsx
@@ -1,9 +1,11 @@
 import {
+  Avatar,
   Box,
   Button,
   Flex,
   Grid,
   Icon,
+  IconButton,
   Image,
   Input,
   Modal,
@@ -19,22 +21,35 @@ import {
   TabPanels,
   Tabs,
   Text,
+  Textarea,
 } from "@chakra-ui/react";
 import { useEffect, useRef, useState } from "react";
 import UploadFile from "./UploadFile";
-import { FaArrowLeft } from 'react-icons/fa';
+import { FaArrowLeft, FaCrop } from 'react-icons/fa';
 import useUploadFileStore from "../../../../store/uploadFileStore";
 import { CloseIcon } from '@chakra-ui/icons';
 import { filterValues } from "./ModalUtils";
 import './filters.css';
+import useAuthStore from "../../../../store/authStore";
+import ReactCrop from 'react-image-crop';
+import 'react-image-crop/dist/ReactCrop.css';
 
 const CreateModal = ({ isOpen, onClose }) => {
   const fileInputRef = useRef(null);
   const [selecedFile, setSelectedFile] = useState(null);
   const [iSt, setISt] = useState(null);
   const image = useUploadFileStore(s=>s.image)
+  const removeImage = useUploadFileStore(s=>s.removeImage)
   const [selectedFilter , setSelectedFilter] = useState("");
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 600);
+  const [showForm , setShowForm] = useState(false);
+  const user = useAuthStore(s=>s.user);
+  const [aspectRatioModalOpen, setAspectRatioModalOpen] = useState(false);
+  const [crop, setCrop] = useState({ aspect: 1 / 1 }); // Initial aspect ratio for cropping
+
+  // State for storing the selected aspect ratio
+  const [selectedAspectRatio, setSelectedAspectRatio] = useState(1);
+  
 
   useEffect(() => {
     const handleResize = () => {
@@ -67,17 +82,28 @@ const CreateModal = ({ isOpen, onClose }) => {
         setISt(true)
     }
   };
+  const handleAspectRatioSelect = (aspectRatio) => {
+    setSelectedAspectRatio(aspectRatio);
+    setAspectRatioModalOpen(false);
+  };
   return (
     <Modal isOpen={isOpen} onClose={onClose}  size={image?isMobile?'full':800:null}>
     <ModalOverlay />
     <ModalContent backgroundColor="#262626" borderRadius={15} w={image?'auto':'full'} h={image?isMobile?'auto':'auto':'400px'} m={'8'}   alignSelf={'center'} overflow={'auto'} >
       <ModalHeader borderBottom="1px solid #363636">
         <Flex alignItems={'center'} justifyContent={'space-between'}>
-          {image && <Icon as={FaArrowLeft} boxSize={6} cursor="pointer" onClick={() => {}} />}
+          {image && <Icon as={FaArrowLeft} boxSize={6} cursor="pointer" onClick={() => {
+            showForm?setShowForm(false):removeImage(null)
+          }} />}
           <Text fontSize="16px" textAlign="center" flex="1">
             {!image ? "Create new post" : "Edit"}
           </Text>
-         {image &&  <Button backgroundColor={'transparent'} color={'blue.500'} _hover={{backgroundColor:"transparent",color:"white"}}>Next</Button>}
+         {!showForm && image &&  <Button backgroundColor={'transparent'} color={'blue.500'} _hover={{backgroundColor:"transparent",color:"white"}} onClick={()=>{
+          setShowForm(true)
+         }}>Next</Button>}
+            {showForm && image &&  <Button backgroundColor={'transparent'} color={'blue.500'} _hover={{backgroundColor:"transparent",color:"white"}} onClick={()=>{
+          setShowForm(true)
+         }}>Share</Button>}
 
         </Flex>
       </ModalHeader>
@@ -86,11 +112,17 @@ const CreateModal = ({ isOpen, onClose }) => {
         {!image ? (
           <UploadFile />
         ) : (
-          <Flex flexDirection={isMobile?'column':null} alignItems={'center'}  overflow={'auto'}>
+          <Flex flexDirection={isMobile?'column':null} alignItems={showForm?null:'center'}  overflow={'auto'}>
             <Box h={'auto'}>
-              <Image alignSelf={'center'} h={isMobile?300:480} src={image} fit="contain" alt="Selected Image" className={selectedFilter} overflow={'auto'} />
+            {/* <ReactCrop
+            src={image}
+            crop={crop}
+            onChange={(newCrop) =>{}}
+            onComplete={(crop, pixelCrop) => {}}
+          /> */}
+              <img height={400} width={400} style={{fit:"contain",aspectRatio:selectedAspectRatio}}     src={image}  alt="Selected Image" className={selectedFilter} overflow={'auto'} />
             </Box>
-         <Box h={400}>
+        {!showForm &&  <Box h={400}>
          <Tabs fit='contain'>
               <TabList>
                 <Tab>
@@ -104,7 +136,7 @@ const CreateModal = ({ isOpen, onClose }) => {
               <TabPanels>
                 <TabPanel>
                <Box overflow={'auto'} h={400}>
-             {   <Grid w={300} templateColumns="repeat(3, 1fr)" gap={4} cursor={'pointer'} overflow={'auto'}  >
+             {<Grid w={300} templateColumns="repeat(3, 1fr)" gap={4} cursor={'pointer'} overflow={'auto'}  >
                     {filterValues.map((filter, index) => (
                       <Box onClick={()=>{setSelectedFilter(filter.class)}} key={index} alignItems={'center'} display={'flex'} flexDirection={'column'}>
                         <Image h={100} src={image} className={filter.class} />
@@ -125,10 +157,47 @@ const CreateModal = ({ isOpen, onClose }) => {
                 </TabPanel>
               </TabPanels>
             </Tabs>
-         </Box>
+         </Box>}
+         {showForm && <Box justifyContent={'flex-start'} alignItems={'flex-start'} minW={'300px'}>
+          <Flex justifyContent={'flex-start'} alignItems={'center'} gap={3} m={2}>
+            <Avatar src={user.profilePicUrl} alt={user.username}/>
+            <Text fontWeight={'bold'}>{user.username}</Text>
+          </Flex>
+          <Box>
+            <Textarea  placeholder="Write a caption.." maxLength={200} border={'null'}/>
+          </Box>
+          </Box>}
           </Flex>
         )}
       </ModalBody>
+      <ModalFooter>
+      <IconButton
+            icon={<FaCrop />}
+            onClick={() => setAspectRatioModalOpen(true)}
+            aria-label="Aspect Ratio"
+            variant="ghost"
+          />
+             <Modal isOpen={aspectRatioModalOpen} onClose={() => setAspectRatioModalOpen(false)} size="xs">
+        <ModalOverlay />
+        <ModalContent backgroundColor="#262626" borderRadius={15} p={4}>
+          <ModalHeader borderBottom="1px solid #363636">
+            <Text fontSize="16px" textAlign="center" flex="1">
+              Select Aspect Ratio
+            </Text>
+          </ModalHeader>
+          <ModalBody>
+            {/* Buttons for different aspect ratios */}
+            <Flex justifyContent="space-around" flexDirection={'column'} gap={2}>
+              <Button backgroundColor={'transparent'} onClick={() => handleAspectRatioSelect('')}>original</Button>
+              <Button backgroundColor={'transparent'} onClick={() => handleAspectRatioSelect(1)}>1:1</Button>
+              <Button backgroundColor={'transparent'} onClick={() => handleAspectRatioSelect(16/9)}>16:9</Button>
+              <Button backgroundColor={'transparent'} onClick={() => handleAspectRatioSelect(4/3)}>4:3</Button>
+              <Button backgroundColor={'transparent'} onClick={() => handleAspectRatioSelect(3/2)}>3:2</Button>
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+      </ModalFooter>
     </ModalContent>
   </Modal>
   
--- src/store/uploadFileStore.js
+++ src/store/uploadFileStore.js
@@ -3,7 +3,8 @@ import {create} from "zustand";
 
 const useUploadFileStore =create((set)=>({
     image:null,
-    setImage:(image)=>set({image})
+    setImage:(image)=>set({image}),
+    removeImage:(image)=>set({image}),
 }))
 
 export default useUploadFileStore;
